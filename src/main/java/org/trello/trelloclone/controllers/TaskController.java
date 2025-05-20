e064b54c6bca1c6b4db180163b1ea84f0bb4a184
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-20 18:17:10 -0300

    Add : Implement TeamController, TaskController, UserController, and BoardController with CRUD operations.`

--- src/main/java/org/trello/trelloclone/controllers/TaskController.java
+++ src/main/java/org/trello/trelloclone/controllers/TaskController.java
@@ -1,4 +1,64 @@
 package org.trello.trelloclone.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.trello.trelloclone.dtos.ResponseObjectJsonDto;
+import org.trello.trelloclone.models.Task;
+import org.trello.trelloclone.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/create_task")
+    public ResponseEntity<ResponseObjectJsonDto> createTask(@RequestBody Task task) {
+        ResponseObjectJsonDto response = taskService.createTask(task);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_task_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> getTaskById(@PathVariable Long id) {
+        ResponseObjectJsonDto response = taskService.getTaskById(id);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_all_tasks")
+    public ResponseEntity<ResponseObjectJsonDto> getAllTasks() {
+        ResponseObjectJsonDto response = taskService.getAllTasks();
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_task_by_{boardId}")
+    public ResponseEntity<ResponseObjectJsonDto> getTasksByBoardId(@PathVariable Long boardId) {
+        ResponseObjectJsonDto response = taskService.getTasksByBoardId(boardId);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_task_by_{userId}")
+    public ResponseEntity<ResponseObjectJsonDto> getTasksByAssignedUserId(@PathVariable Long userId) {
+        ResponseObjectJsonDto response = taskService.getTasksByAssignedUserId(userId);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @PutMapping("/update_task_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        ResponseObjectJsonDto response = taskService.updateTask(id, task);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @DeleteMapping("/delete_task_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> deleteTask(@PathVariable Long id) {
+        ResponseObjectJsonDto response = taskService.deleteTask(id);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
 }
+
--- src/main/java/org/trello/trelloclone/controllers/UserController.java
+++ src/main/java/org/trello/trelloclone/controllers/UserController.java
@@ -1,4 +1,57 @@
 package org.trello.trelloclone.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.trello.trelloclone.dtos.ResponseObjectJsonDto;
+import org.trello.trelloclone.models.User;
+import org.trello.trelloclone.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create_user")
+    public ResponseEntity<ResponseObjectJsonDto> createUser(@RequestBody User user) {
+        ResponseObjectJsonDto response = userService.createUser(user);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_user_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> getUserById(@PathVariable Long id) {
+        ResponseObjectJsonDto response = userService.getUserById(id);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_user_by_{email}")
+    public ResponseEntity<ResponseObjectJsonDto> getUserByEmail(@PathVariable String email) {
+        ResponseObjectJsonDto response = userService.getUserByEmail(email);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/get_all_users")
+    public ResponseEntity<ResponseObjectJsonDto> getAllUsers() {
+        ResponseObjectJsonDto response = userService.getAllUsers();
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @PutMapping("/update_user_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> updateUser(@PathVariable Long id, @RequestBody User user) {
+        ResponseObjectJsonDto response = userService.updateUser(id, user);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @DeleteMapping("/delete_user_by_{id}")
+    public ResponseEntity<ResponseObjectJsonDto> deleteUser(@PathVariable Long id) {
+        ResponseObjectJsonDto response = userService.deleteUser(id);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
 }
